46e950ff1df6a9998f9a0306e371fad983767791
ASUS-A455l <nuraniiin38@gmail.com>
2016-12-23 21:57:22 +0700

    1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl504132231/indonesiaku/model/Hotel.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xirpl504132231.indonesiaku.model;
+
+import android.graphics.drawable.Drawable;
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
